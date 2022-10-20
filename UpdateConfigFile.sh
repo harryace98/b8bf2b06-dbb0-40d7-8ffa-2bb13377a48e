0710ea48e64bf83aa3043b253c3986550da2d34e
Henry Acevedo <harryramirez69@gmail.com>
2022-10-20 15:31:13 -0500

    update the directory target of the config file

--- UpdateConfigFile.sh
+++ UpdateConfigFile.sh
@@ -5,14 +5,14 @@ sleep 1
 _now=$(date +"%Y-%m-%dT%H:%M:%S")
 echo "Actual date:  " "$_now"
 # Moving into the directory to make the upatade.
-cd /home/pi/gespline-api/wwwroot/data/
+cd /home/pi/Configurations/Setting/
 # Saving the previus configurations in a backUp
 sudo mv config.json config.json.bk
 # Getting the most recent version of the configurations file.
 printf "Downloading the current version of the configuration file..."
 curl https://raw.githubusercontent.com/harryace98/b8bf2b06-dbb0-40d7-8ffa-2bb13377a48e/main/config.json > config.json
 # Giving the correct file permission 
-sudo chown pi:pi /home/pi/gespline-api/wwwroot/data/config.json
+sudo chown pi:pi /home/pi/Configurations/Setting/config.json
 # Kill the java process for reset the app.
 printf "Killing the actual IoT Capture Process... "
 sudo killall java 
